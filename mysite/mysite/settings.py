b2013671ed134da0bf17d371a242e52cc01532f5
Chun Lu <chun.lu.dev@gmail.com>
2023-10-10 20:47:36 -0700

    Added user registation

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "food",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -93,9 +95,6 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
@@ -118,7 +117,13 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "food:index"
+LOGIN_URL = "login"
+MEDIA_ROOT = os.path.join(BASE_DIR, "pictures")
+MEDIA_URL = "/pictures/"
